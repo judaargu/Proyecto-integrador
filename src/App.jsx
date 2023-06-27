9404185cc261f8e24e0e812be9749a6cd081c58b
judaargu <judaargu@hotmail.com>
2023-06-27 14:57:18 -0500

    Primer commit integrador M2

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Cards from './components/Cards.jsx';
+import SearchBar from './components/SearchBar.jsx';
+import characters from './data';
+
+function App() {
+   return (
+      <div className='App'>
+         <SearchBar onSearch={(characterID) => window.alert(characterID)} />
+         <Cards characters={characters} />
+      </div>
+   );
+}
+
+export default App;
